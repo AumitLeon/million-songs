8c3d1ea664501d36f25a02699730b3e125581259
Mariana Echeverria <marianaecheverria@marianas-mbp-2.middlebury.edu>
2017-11-21 09:34:43 -0500

    Forgot to add features in line 23

--- get_data.py
+++ get_data.py
@@ -18,9 +18,9 @@ indir = '/mnt/c/Users/Aumit/Desktop/final-proj/MillionSongSubset/data/B/'
 # Open the CSV file we will write to
 with open("output.csv", 'wb') as csvfile:
     # Column headers
-    # Currently only 2 features being extracted
+    # Currently only 4 features being extracted
     # We can add as many as we want, just seperate with commas
-    csvfile.write("artist_name, title")
+    csvfile.write("artist_name, title, artist_location, release")
     csvfile.write("\n")
 
     # Recursively visit each sub-dir till we reach the h5 files
