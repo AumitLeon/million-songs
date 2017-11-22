891c42de0dca71d58c3e6fda8415bc8cc06dabdb
AumitLeon <aumitleon1@gmail.com>
2017-11-21 19:49:30 -0500

    added some more features

--- get_data.py
+++ get_data.py
@@ -21,10 +21,11 @@ with open("output.csv", 'wb') as csvfile:
     # Column headers
     # Currently only 4 features being extracted
     # We can add as many as we want, just seperate with commas
-    csvfile.write("artist_name, title, artist_location, release")
+    csvfile.write("artist_name, title, artist_location, release, hotttness, familiarity, danceability, duration, energy, loudness, year, tempo")
     csvfile.write("\n")
 
     # Recursively visit each sub-dir till we reach the h5 files
+    # Strip punctuation from features that are strings
     for root, dirs, filenames in os.walk(indir):
         for f in filenames:
             #log = open(os.path.join(root, f),'r')
@@ -53,15 +54,43 @@ with open("output.csv", 'wb') as csvfile:
             release_song = hdf5_getters.get_release(h5)
             release = release_song.translate(None, string.punctuation)
 
+            # Get artist HOTTTNESSSSSS
+            hotttness = hdf5_getters.get_artist_hotttnesss(h5)
+
+            # Get artist familiarity 
+            familiarity = hdf5_getters.get_artist_familiarity(h5)
+
+            # Get danceability
+            danceability = hdf5_getters.get_danceability(h5)
+
+            # Get duration
+            duration = hdf5_getters.get_duration(h5)
+
+            # Get energy
+            #*****useless... column is filled with 0's?
+            energy = hdf5_getters.get_energy(h5)
+
+            # Get loudness
+            loudness = hdf5_getters.get_loudness(h5)
+
+            # Get year
+            year = hdf5_getters.get_year(h5)
+
+            # Get tempo
+            tempo = hdf5_getters.get_tempo(h5)
+
+            num_songs = hdf5_getters.get_num_songs(h5)
+
             # Close the h5 file
             h5.close()
 
             # Write to the CSV file
-            csvfile.write(artist + "," + title + "," + artist_loc + "," + release)
+            csvfile.write(artist + "," + title + "," + artist_loc + "," + release + "," + str(hotttness) + "," + str(familiarity) + "," + str(danceability) + "," + str(duration) + "," + str(energy) + "," + str(loudness) + "," + str(year) + "," + str(tempo))
             csvfile.write("\n")
 
             # Print the current song and arists: 
             print title + " by " + artist_name
+            #print str(num_songs)
             # Move on to the next h5 file
             print
             print
