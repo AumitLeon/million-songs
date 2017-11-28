a4039ac44584ff416569f6c7bb8be1415f2a438a
AumitLeon <aumitleon1@gmail.com>
2017-11-28 00:20:29 -0500

    feature scaling + lists to numpy arrays

--- nnet-year-prediction.py
+++ nnet-year-prediction.py
@@ -17,6 +17,7 @@
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_recall_fscore_support
+from sklearn import preprocessing
 import numpy as np
 
 # Scikit learn gives a depreaction warning for the rank function
@@ -35,27 +36,40 @@ with open(filename, 'r') as f:
     for line in f:
         content = line.split(",")
         
-        labels.append(int(content[0]))
+        labels.append(content[0])
 
         content.pop(0)
 
-        content = [float(elem) for elem in content]
+        # If we wanted pure lists
+        #content = [float(elem) for elem in content]
+        #content = map(float, content)
 
         # If we want a list of numpy arrays, not necessary
         #npa = np.asarray(content, dtype=np.float64)
 
         examples.append(content)
 
+# Turning lists into numpy arrays
+total_array = np.array(examples)
+
+# Scale the features so they have 0 mean
+total_scaled = preprocessing.scale(total_array)
+
+# Numpy array of the labels 
+total_labels = np.array(labels)
 
 # Split training and test:
 # Increase or decrease these sizes
 # Currently using first 10000 examples as training data
 # Last 1000 as test data
-training_examples = examples[:10000]
-training_labels = labels[:10000]
+training_examples = total_scaled[:10000]
+#training_examples = random.sample(total_array, 10)
+training_labels = total_labels[:10000]
 
-test_examples = examples[-1000:]
-test_labels = labels[-1000:]
+# Use the following 1000 examples as text examples
+test_examples = total_scaled[10000:11000]
+test_labels = total_labels[10000:11000]
+
 
 # Debugging
 #X = [[0., 0.], [1., 1.]]
@@ -63,8 +77,12 @@ test_labels = labels[-1000:]
 
 # Solver can be lbfgs, sgd, adam
 # We can tune the hidden layers sizes
-clf = MLPClassifier(solver='sgd', alpha=1e-5,
-                     hidden_layer_sizes=(100, 100, 100, 100), random_state=1)
+# 50, 50, 20 with 100 batch size worked well
+# 0.0000001 9.4%
+# 0.00001
+clf = MLPClassifier(solver='sgd', alpha=0.00001,
+                     hidden_layer_sizes=(50, 50, 20), random_state=1, batch_size = 100, learning_rate="adaptive", max_iter=200,
+                     momentum=0.9)
 
 clf.fit(training_examples, training_labels)                         
 
@@ -79,5 +97,6 @@ clf.fit(training_examples, training_labels)
 y_pred = clf.predict(test_examples)
 accuracy = accuracy_score(test_labels, y_pred) * 100
 print "Accuracy of the model is: " + str(accuracy) + "%"
+print "Score: " + str(clf.score(test_examples, test_labels))
 print "Precision, recall and f-score:"
 print precision_recall_fscore_support(test_labels, y_pred, average="micro")
